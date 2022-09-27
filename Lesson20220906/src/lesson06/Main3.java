7048ae18802d8ff4239e27c39efe81180e9dc9ce
A-SUNAGAWA <suna201022@gmail.com>
2022-09-27 21:59:05 +0900

    20220927 update Lesson06

--- Lesson20220906/src/lesson06/Main3.java
+++ Lesson20220906/src/lesson06/Main3.java
@@ -1,21 +1,17 @@
 package lesson06;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main3 {
 
 	public static void main(String[] args) {
 
-		int someNum = 3;
-		String[] copy = new String[someNum];
-		String str = "Hello";
-
-		for(int i=0;i<copy.length;i++) {
-			copy[i]=str;
+		int[] nums = {new Random().nextInt(21),new Random().nextInt(21),new Random().nextInt(21)};
+		Arrays.sort(nums);
+		for(int i:nums) {
+			System.out.print(i + " ");
 		}
-
-		for(String s:copy) {
-			System.out.println(s);
-		}
-
 	}
 
 }
